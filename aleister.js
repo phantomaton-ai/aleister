c830759d46d55196d356c9eaf2d6d9998ea46ba0
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-17 08:34:48 -0700

    progress

--- aleister.js
+++ aleister.js
@@ -18,18 +18,26 @@ export default function aleister(Class) {
   const comments = [];
   const options = {ecmaVersion: 2022, sourceType: 'module', onComment: comments};
   const parsed = acorn.parse(Class.toString(), options);
-  const classbody = find('ClassBody', parsed);
-  //console.log(parsed, comments);
-  const candidates = comments.filter(
+  const root = find('ClassBody', parsed);
+  const methods = root.body.filter(({ type }) => type === 'MethodDefinition');
+  const commands = comments.filter(
     ({ type }) => type === 'Block'
   ).map(({ value, start, end }) => {
-    
-    return doctrine.parse(value, { unwrap: true });
+    const method = methods.find(m => m.start > end);
+    return { method, value };
+  }).filter(({ method }) => method).map(({ method, value }) => {
+    const name = method.key.name;
+    const documentation = doctrine.parse(value, { unwrap: true });
+    const description = documentation.description;
+    return { name, description };
   });
   console.log(
     JSON.stringify(parsed, null, 2),
     comments,
-    JSON.stringify(candidates, null, 2),
-    classbody
+    JSON.stringify(commands, null, 2),
+    root,
+    methods,
+    commands
   );
+  return { commands };
 }
