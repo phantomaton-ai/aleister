c75abbcf9b58d708f40d3d04574a309de1e04cf9
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-17 08:26:01 -0700

    doctrine/acorn

--- aleister.js
+++ aleister.js
@@ -1,6 +1,35 @@
 import * as acorn from 'acorn';
+import * as doctrine from 'doctrine';
 
+function first(array, fn) {
+  for (const element of array) {
+    const value = fn(element);
+    if (value) return value;
+  }
+}
+
+function find(type, tree) {
+  if (Array.isArray(tree)) return first(tree, subtree => find(type, subtree));
+  if (tree.type === type) return tree;
+  if (tree.body) return find(type, tree.body);
+}
 
 export default function aleister(Class) {
-   
+  const comments = [];
+  const options = {ecmaVersion: 2022, sourceType: 'module', onComment: comments};
+  const parsed = acorn.parse(Class.toString(), options);
+  const classbody = find('ClassBody', parsed);
+  //console.log(parsed, comments);
+  const candidates = comments.filter(
+    ({ type }) => type === 'Block'
+  ).map(({ value, start, end }) => {
+    
+    return doctrine.parse(value, { unwrap: true });
+  });
+  console.log(
+    JSON.stringify(parsed, null, 2),
+    comments,
+    JSON.stringify(candidates, null, 2),
+    classbody
+  );
 }
